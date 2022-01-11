abb2a84f6a02228483bd374054741d2588279f61
VishalS-avasoft <95081412+VishalS-avasoft@users.noreply.github.com>
2022-01-11 14:41:59 +0530

    Add UI for Insert New Store (#26)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { FC } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import DashBoard from './pages/DashBoard/DashBoard';
 import HomePage from './pages/HomePage/HomePage';
+import InsertPage from './pages/InsertPage/InsertPage';
 
 const App: FC = () => (
   <Router>
     <Routes>
       <Route path="/dashboard" element={<DashBoard />} />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/" element={<HomePage />} />
+      <Route path="/insert-store" element={<InsertPage />} />
     </Routes>
   </Router>
 );
